0ee07ee00d311d9e60d8a86752f70018a018a89c
jhekz <ramadhanfhujjar@gmail.com>
2019-05-01 11:59:07 +0800

    faker seed

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -6,7 +6,11 @@ use App\Model\Product;
 use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
-    return [
-        //
-    ];
+	return [
+		'name' => $faker->word,
+		'detail' => $faker->paragraph,
+		'price' => $faker->numberBetween(100,1000),
+		'stock' => $faker->randomDigit,
+		'discount' => $faker->numberBetween(2,30)
+	];
 });
